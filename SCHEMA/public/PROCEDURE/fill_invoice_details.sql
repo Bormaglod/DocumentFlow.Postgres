1d6f9631990483781ec287f8a854e698a402e7f2
Тепляшин Сергей <sergio.teplyashin@gmail.com>
2021-01-11 23:47:39 +0400

    Версия 2021.01.11

--- SCHEMA/public/PROCEDURE/fill_invoice_details.sql
+++ SCHEMA/public/PROCEDURE/fill_invoice_details.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE FUNCTION public.fill_invoice_detail(owner_code_id character varying, from_copy_id uuid, invoice_id uuid) RETURNS void
+CREATE OR REPLACE PROCEDURE public.fill_invoice_details(entity_code character varying, from_copy_id uuid, invoice_id uuid)
     LANGUAGE plpgsql
     AS $$
 begin
-	case owner_code_id
+	case entity_code
 		when 'purchase_request' then
 			delete from invoice_receipt_detail where owner_id = invoice_id;
 			insert into invoice_receipt_detail (owner_id, goods_id, amount, price, cost, tax, tax_value, cost_with_tax) select invoice_id as owner_id, goods_id, amount, price, cost, tax, tax_value, cost_with_tax from purchase_request_detail where owner_id = from_copy_id;
@@ -17,4 +17,4 @@ begin
 end;
 $$;
 
-ALTER FUNCTION public.fill_invoice_detail(owner_code_id character varying, from_copy_id uuid, invoice_id uuid) OWNER TO postgres;
+ALTER PROCEDURE public.fill_invoice_details(entity_code character varying, from_copy_id uuid, invoice_id uuid) OWNER TO postgres;
