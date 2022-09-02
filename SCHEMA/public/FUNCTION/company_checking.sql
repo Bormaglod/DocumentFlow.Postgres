f2660606ee4d4366d3e60c0031f2da32651e3cdb
Тепляшин Сергей Васильевич <sergio.teplyashin@yandex.ru>
2022-09-02 21:59:34 +0400

    Версия 2022.9.2

--- SCHEMA/public/FUNCTION/company_checking.sql
+++ SCHEMA/public/FUNCTION/company_checking.sql
@@ -4,18 +4,20 @@ CREATE OR REPLACE FUNCTION public.company_checking() RETURNS trigger
 declare 
 	code numeric;
 begin
-	code = coalesce(new.inn, 0);  
+	code := coalesce(new.inn, 0);  
 	if ((code > 0) and (not contractor_test_inn(code))) then
 		raise exception 'Некорректное значение ИНН';
 	end if;
 
-	code = coalesce(new.okpo, 0);
+	code := coalesce(new.okpo, 0);
 	if ((code > 0) and (not contractor_test_okpo(code))) then
 		raise exception 'Некорректное значение ОКПО';
 	end if;
 
-	if (new.parent_id is null) then
-		raise exception 'Выберите группу. Контрагент должен быть или в группе "Юридические лица", или в группе "Физические лица".';
+	if (TG_TABLE_NAME = 'contractor') then
+		if (new.parent_id is null) then
+			raise exception 'Выберите группу. Контрагент должен быть или в группе "Юридические лица", или в группе "Физические лица".';
+		end if;
 	end if;
 
 	return new;
