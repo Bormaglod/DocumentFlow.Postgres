faf3e556481d047a7de5dda09769757ad7f37888
Тепляшин Сергей Васильевич <sergio.teplyashin@yandex.ru>
2022-11-15 21:43:06 +0400

    Версия 2022.11.15

--- SCHEMA/public/FUNCTION/average_price.sql
+++ SCHEMA/public/FUNCTION/average_price.sql
@@ -1,12 +1,35 @@
 CREATE OR REPLACE FUNCTION public.average_price(product_id uuid, relevance_date timestamp with time zone) RETURNS numeric
-    LANGUAGE sql
+    LANGUAGE plpgsql
     AS $$
+declare
+	avg_price numeric;
+begin
 	select sum(operation_summa * sign(amount)) / sum(amount)
+		into avg_price
 		from balance_product
 		where
 			reference_id = product_id and
 			document_date < relevance_date
 		having sum(amount) != 0;
+	
+	if (coalesce(avg_price, 0) = 0) then
+		select sum(bp.operation_summa) / sum(bp.amount)
+			into avg_price
+			from balance_product bp
+			join document_type dt on dt.id = bp.document_type_id 
+			where
+				bp.reference_id = product_id and
+				bp.document_date <= relevance_date and
+				dt.account_avg
+			having sum(bp.amount) != 0;
+			
+		if (coalesce(avg_price, 0) = 0) then
+			select coalesce(price, 0) into avg_price from product where id = product_id;
+		end if;
+	end if;
+
+	return avg_price;
+end;
 $$;
 
 ALTER FUNCTION public.average_price(product_id uuid, relevance_date timestamp with time zone) OWNER TO postgres;
--- SCHEMA/public/TABLE/document_type.sql
+++ SCHEMA/public/TABLE/document_type.sql
@@ -1,12 +1,15 @@
 CREATE TABLE public.document_type (
 	id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
 	code character varying(30) NOT NULL,
-	document_name character varying(250)
+	document_name character varying(250),
+	account_avg boolean
 );
 
 ALTER TABLE public.document_type OWNER TO postgres;
 
 GRANT SELECT ON TABLE public.document_type TO users;
+
+COMMENT ON COLUMN public.document_type.account_avg IS 'Учитывать при расчёте средней цены';
 
 --------------------------------------------------------------------------------
 
