f419790cad739f960434a9b87b527025ee215162
Тепляшин Сергей Васильевич <sergio.teplyashin@yandex.ru>
2023-11-17 21:17:54 +0400

    Версия 2023.11.17

--- SCHEMA/public/FUNCTION/iif.sql
+++ SCHEMA/public/FUNCTION/iif.sql
@@ -11,3 +11,19 @@ end;
 $$;
 
 ALTER FUNCTION public.iif(boolean_expression boolean, true_value numeric, false_value numeric) OWNER TO postgres;
+
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION public.iif(boolean_expression boolean, true_value character varying, false_value character varying) RETURNS character varying
+    LANGUAGE plpgsql IMMUTABLE
+    AS $$
+begin
+	if (boolean_expression) then
+    	return true_value;
+	else
+    	return false_value;
+	end if;
+end;
+$$;
+
+ALTER FUNCTION public.iif(boolean_expression boolean, true_value character varying, false_value character varying) OWNER TO postgres;
